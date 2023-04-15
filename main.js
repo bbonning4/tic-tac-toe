88c60f6c50a4955cb96d372693ca81bf094eca44
Bo <bbonning42@gmail.com>
2023-04-15 01:43:19 -0500

    game w/ min requirements

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // constants
 const SYMBOLS = {
-    '0': null,
+    '0': '',
     '1': 'X',
     '-1': 'O'
 };
@@ -33,13 +33,67 @@ function init() {
     render();
 }
 
-function render() {
-    renderBoard();
+function render(sqrIdx) {
+    renderBoard(sqrIdx);
     renderMessage();
     // change/disable the div:hover for divs not 0
     renderControls();
 }
 
 function placeSymbol(evt) {
+    // place turn player's symbol on div if it doesn't already have one
+    const sqr = evt.target;
+    const sqrIdx = sqrEl.indexOf(sqr);
+    // Guard
+    if(sqrIdx === -1) return;
+    const xIdx = sqrIdx % 3;
     
+    let yIdx;
+    if(sqrIdx < 3) {
+        yIdx = 2;
+    }
+    else if(sqrIdx < 6) {
+        yIdx = 1;
+    }
+    else {
+        yIdx = 0;
+    }
+    
+    if(board[xIdx][yIdx] === 0) {
+        board[xIdx][yIdx] = turn;
+    }
+    else {
+        return;
+    }
+
+    turn *= -1;
+    winner = getWinner(xIdx, yIdx);
+    render(sqrIdx);
+}
+
+function renderBoard(sqrIdx) {
+    if(sqrIdx || sqrIdx === 0) {
+        board.forEach(function(xIdx) {
+            xIdx.forEach(function() {
+                sqrEl[sqrIdx].innerText = SYMBOLS[turn];
+            });
+        });
+    }
+    else {
+        sqrEl.forEach(function(sqr) {
+            sqr.innerText = '';
+        });
+    }
+}
+
+function getWinner() {
+
+}
+
+function renderMessage() {
+
+}
+
+function renderControls() {
+
 }
