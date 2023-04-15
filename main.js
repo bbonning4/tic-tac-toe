10a865b263972e02a29a68e21e7ec02a714730e3
Bo <bbonning42@gmail.com>
2023-04-15 14:46:09 -0500

    added features

--- main.js
+++ main.js
@@ -14,14 +14,18 @@ const messageEl = document.querySelector('h1');
 const resetBtn = document.querySelector('button');
 const sqrEl = [...document.querySelectorAll('#board > div')];
 
-// event listeners
-document.getElementById('board').addEventListener('click', placeSymbol);
+// event listeners; 2nd one added in init 
 resetBtn.addEventListener('click', init);
 
 // functions
 init();
 
 function init() {
+    document.getElementById('board').addEventListener('click', placeSymbol);
+    sqrEl.forEach((sqr) => {
+        sqr.style.setProperty('--div-bg-color', 'lightgrey');
+    });
+
     board = [
         [0, 0, 0],
         [0, 0, 0],
@@ -37,6 +41,7 @@ function render(sqrIdx) {
     renderBoard(sqrIdx);
     renderMessage();
     // change/disable the div:hover for divs not 0
+    // if there is a winner or 'draw'
     renderControls();
 }
 
@@ -74,8 +79,8 @@ function placeSymbol(evt) {
 function renderBoard(sqrIdx) {
     if(sqrIdx || sqrIdx === 0) {
         board.forEach(function(xIdx) {
-            xIdx.forEach(function() {
-                sqrEl[sqrIdx].innerText = SYMBOLS[turn];
+            xIdx.forEach(function(yIdx) {
+                sqrEl[sqrIdx].innerText = SYMBOLS[-1*turn];
             });
         });
     }
@@ -86,14 +91,86 @@ function renderBoard(sqrIdx) {
     }
 }
 
-function getWinner() {
+function getWinner(x, y) {
+    //next
+    return checkVerticalWin(x, y) ||
+        checkHorizontalWin(x, y) ||
+        checkDiagonalWin(x, y) ||
+        checkDraw();
+}
 
+function checkVerticalWin(x, y) {
+    return countAdjacent(x, y, 0, 1) === 2 ? board[x][y] : null;
+}
+
+function checkHorizontalWin(x, y) {
+    return countAdjacent(x, y, 1, 0) === 2 ? board[x][y] : null;
+}
+// missing NW>SE diagonal win
+function checkDiagonalWin(x, y) {
+    return countAdjacent(x, y, 1, 1) === 2 ||
+        countAdjacent(x, y, 1, -1) === 2 ? board[x][y] : null;
+}
+
+function checkDraw() {
+    let fullColumn = 0;
+    board.forEach(function(xIdx) {
+        if(xIdx.includes(0)) {}
+        else {fullColumn++;}
+    });
+    return fullColumn === 3 ? 'draw' : null;
+}
+
+function countAdjacent(x, y, dx, dy) {
+    const player = board[x][y];
+    let count = 0;
+
+    // negative offset
+    let xNegative = x - dx;
+    let yNegative = y - dy;
+    // positive offset
+    x += dx;
+    y += dy;
+    while(board[x] !== undefined &&
+        board[x][y] !== undefined &&
+        board[x][y] === player) {
+            count++
+            x += dx;
+            y += dy;
+        }
+    while(board[xNegative] !== undefined &&
+        board[xNegative][yNegative] !== undefined &&
+        board[xNegative][yNegative] === player) {
+            count++;
+            xNegative -= dx;
+            yNegative -= dy;
+        }
+    return count;
 }
 
 function renderMessage() {
-
+    if(winner) {
+        if(winner === 'draw') {
+            messageEl.innerHTML = `It's a Draw!`;
+        }
+        else {
+            messageEl.innerHTML = `${SYMBOLS[-1*turn]} Wins!`
+        }
+    }
+    else {
+        messageEl.innerHTML = `${SYMBOLS[turn]}'s Turn`;
+    }
 }
 
 function renderControls() {
-
+    if(winner !== null) {
+        document.getElementById('board').removeEventListener('click', placeSymbol);
+    }
+    sqrEl.forEach(function(sqr) {
+        const disableHover = (sqr.innerText === 'O') ||
+        (sqr.innerText === 'X') || winner;
+        if(disableHover) {
+            sqr.style.setProperty('--div-bg-color', 'darkgrey');
+        }
+    });
 }
